8ded7dc3f69015f52f7ad008a663f6eb125179c6
paniznr <45252666+paniznr@users.noreply.github.com>
2019-09-17 18:01:13 -0700

    Create student.cpp

---
+++ student.cpp
@@ -0,0 +1,22 @@
+//student.cpp to implement your classes
+#include "student.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+class Student
+{
+ public: 
+	string first_name;
+	string last_name;
+	float CGPA;
+	int res_score;
+
+}
+
+
+
+}
+
