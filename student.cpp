9256030142da983960143f9175dd1cc274424cd7
paniznr <45252666+paniznr@users.noreply.github.com>
2019-09-18 14:09:05 -0700

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,22 +1,23 @@
 //student.cpp to implement your classes
 #include "student.hpp"
 #include <iostream>
+#include <string>
 using namespace std;
-
-int main()
-{
 
 class Student
 {
- public: 
-	string first_name;
-	string last_name;
-	float CGPA;
-	int res_score;
+public:
+        string first_name;
+        string last_name;
+        float CGPA;
+        int res_score;
+        int app_id;
+
+        private:
+        //functions for error checking  
+
 
 }
 
 
-
-}
-
+class InternationalStudent public: Student
