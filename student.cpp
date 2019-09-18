e37bb7375f678ba8d2791208cfe31b64fd37dc36
paniznr <45252666+paniznr@users.noreply.github.com>
2019-09-18 15:13:48 -0700

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -3,21 +3,5 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-class Student
-{
-public:
-        string first_name;
-        string last_name;
-        float CGPA;
-        int res_score;
-        int app_id;
-
-        private:
-        //functions for error checking  
-
-
-}
-
-
-class InternationalStudent public: Student
+ 
+//this is where we would make the functions
