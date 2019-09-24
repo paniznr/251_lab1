9c1ade6144ef5458bbc090c2a55490f6bb1e493f
Nikita <45350616+nktac@users.noreply.github.com>
2019-09-24 16:24:34 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,8 @@ int main(){
   //Read the domestic-stu.txt file and exit if failed
   string line;
   ifstream domesticFile("domestic-stu.txt");
-  if(!domesticFile.is_open()) {
+  if(!domesticFile.is_open()) 
+  {
     cout << "Unable to open file domestic-stu.txt" << endl;
     return -1;
   }
@@ -94,7 +95,7 @@ int main(){
    *use get and set functions to manipulate your object, and
    *print the object content to the screen
    */
-  int stu_count = 1;
+  int stu_Count = 1;
   while(getline(internationalFile, line)) 
   {
     /*process each line, each field is separated by a comma.
@@ -102,13 +103,13 @@ int main(){
     */
     istringstream ss(line);
 
-    string FirstName, LastName, Country, CGPA, ResearchScore, Reading, Listening, Speaking, Writing;
+    string FirstName, LastName, Country, s_CGPA, s_ResearchScore, s_Reading, s_Listening, s_Speaking, s_Writing;
     float CGPA;
     int ResearchScore;
-    int Reading;
-    int Listening;
-    int Speaking;
-    int Writing;
+    int reading;
+    int listening;
+    int speaking;
+    int writing;
 
     //get FirstName separated by comma
     getline(ss, FirstName, ',');
@@ -120,35 +121,35 @@ int main(){
     getline(ss, Country, ',');
 
     //get CGPA separated by comma, and convert string to float
-    getline(ss, CGPA, ',');
-    CGPA = atof(CGPA.c_str());
+    getline(ss, s_CGPA, ',');
+    CGPA = atof(s_CGPA.c_str());
     
     //get ResearchScore separated by comma, and convert it to int
-    getline(ss, ResearchScore, ',');
-    ResearchScore = atoi(ResearchScore.c_str());
+    getline(ss, s_ResearchScore, ',');
+    ResearchScore = atoi(s_ResearchScore.c_str());
 	  
     //get Reading separated by comma, and convert it to int
-    getline(ss, Reading, ',');
-    Reading = atoi(Reading.c_str());
+    getline(ss, s_Reading, ',');
+    reading = atoi(s_Reading.c_str());
 	  
     //get Listening separated by comma, and convert it to int
-    getline(ss, Listening, ',');
-    Listening = atoi(Listening.c_str());
+    getline(ss, s_Listening, ',');
+    listening = atoi(s_Listening.c_str());
 	  
     //get Speaking separated by comma, and convert it to int
-    getline(ss, Speaking, ',');
-    Speaking = atoi(Speaking.c_str());
+    getline(ss, s_Speaking, ',');
+    speaking = atoi(s_Speaking.c_str());
 
     //get Writing separated by comma, and convert it to int
-    getline(ss, Writing, ',');
-    Writing = atoi(Writing.c_str());
+    getline(ss, s_Writing, ',');
+    writing = atoi(s_Writing.c_str());
 	  
     //print the student info to the screen
     cout << "International student " << stu_count << " " << FirstName << " " 
 	 << LastName << " from " << Country << " country has cgpa of "
-	 << CGPA << ", research score of " << ResearchScore << ", reading score of " << Reading
-	 << ", listening score of " << Listening << ", speaking score of " << Speaking << ", and writing score of "
-	 << Writing << endl;
+	 << CGPA << ", research score of " << ResearchScore << ", reading score of " << reading
+	 << ", listening score of " << listening << ", speaking score of " << speaking << ", and writing score of "
+	 << writing << endl;
 	 
     stu_count++;
   }
