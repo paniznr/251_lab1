d96188f4d0c4273c136edea95dd70570b0df2d3d
paniznr <45252666+paniznr@users.noreply.github.com>
2019-09-25 20:51:45 -0700

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -1,35 +1,53 @@
-//header file student.hpp to declare your classes
+//Nikita Cutinho
+//Paniz Najjarrezaparast
 
-//put constructors in header file??
-//should we define private errorchecking functions in .hpp
+//-------------------------student.hpp---------------------------------
+//This file is used to declare all of our classes, which includes their functions and variables such as the error checkers and gets and sets.
+
 
 #include <iostream>
 #include <string>
 using namespace std;
 
+//-------------------------ToeflScore---------------------------------
+
 class ToeflScore
 {
-        private:
+          //ToeflScore private variables
+          //These variables are private because we don't want to allow the user to change the values.
+
+          private:
                 int reading;
                 int listening;
                 int speaking;
                 int writing;
                 int total;
+          //ToeflScore error checkers
+          //The error checking functions are private as well, since we don't want the user to see how we detect the errors, only that there is one.
 
                 void reading_check(int read);
                 void listening_check(int listen);
                 void speaking_check(int speak);
                 void writing_check(int write);
                 void total_check(int tot);
+
+          //ToeflScore public functions
+
         public:
+                //All get/set functions are public, since we want the user to access them and edit them.
+
+                //The parameters of ToeflScore include the variables which are specific to the toefl score,such as the specific test scores, as mentioned in the instructions.
                 ToeflScore(int read, int listen, int speak, int write);
+                //ToeflScore default constructor, in case there is a situation where no arguments are given.
                 ToeflScore();
+                //ToeflScore get functions
                 int getReading();
                 int getListening();
                 int getSpeaking();
                 int getWriting();
                 int getTotal();
-                //just using shortened names
+                //The inputs 'read,' 'listen,' etc. are shortened names of the function ('Reading,' 'Listening,' etc)
+                //ToeflScore set functions
                 void setReading(int read);
                 void setListening(int listen);
                 void setSpeaking(int speak);
@@ -39,74 +57,85 @@ class ToeflScore
 
 };      
 
+//-------------------------Student---------------------------------
+
 
 class Student
 {
-public:
+        private:
+                //Student error checking functions
+                void cgpa_check(float cgpa);
+                void res_score_check(int Res); 
+                void id_check(int App);
 
-        Student(string FN, string LN, float cgpa, int Res, int App);
-        //default constructor
-        //so that if called with no arguments, compiler knows what to do.
-        Student();
+                //Student private variables
 
-        string getFirst_name();
-        string getLast_name();
-        float getCGPA();
-        int getRes_score();
-        int getApp_id();
+                string First_name;
+                string Last_name;
+                float CGPA;
+                int Res_score;
+                int App_id;
 
-        //using shortened versions of names
-        void setFirst_name(string FN);
-        void setLast_name(string LN);
-        void setCGPA(float CG);
-        void setRes_score(int Res);
-        void setApp_id(int App);
+        public:
 
-       
+                //Student main constructor
+                Student(string FN, string LN, float cgpa, int Res, int App);
+                //Student default constructor
+                //so that if called with no arguments, compiler knows what to do.
+                Student();
 
-private:
-        //functions for error checking 
-        void cgpa_check(float cgpa);
-        //or should i check CGPA
-        void res_score_check(int Res); 
+                //Student get functions
+                string getFirst_name();
+                string getLast_name();
+                float getCGPA();
+                int getRes_score();
+                int getApp_id();
 
-        void id_check(int App);
+                //Student set functions
+                void setFirst_name(string FN);
+                void setLast_name(string LN);
+                void setCGPA(float CG);
+                void setRes_score(int Res);
+                void setApp_id(int App);
 
-        string First_name;
-        string Last_name;
-        float CGPA;
-        int Res_score;
-        int App_id;
 };
 
+//-------------------------InternationalStudent-------------
 
 class InternationalStudent : public Student
 {
-private:
-        string Country;
-        ToeflScore Score;
-     //   void intStu_score_check(int Score);
-
-public: 
-        InternationalStudent(string Cnt,ToeflScore score);
-        //Student(string FN, string LN, float cgpa, int Res, int App);
-        InternationalStudent();
-        string getCountry();
-
-        int getScore();
-        void setCountry(string Cnt);
-        //void setToeflScore(int score);
+         //InternationalStudent private variables
+        private:
+                string Country;
+                ToeflScore Score;
+        //InternationalStudent public functions
+        public: 
+                InternationalStudent(string Cnt,ToeflScore score);
+                //InternationalStudent default constructor
+                InternationalStudent();
+                //InternationalStudent get functions
+                string getCountry();
+                int getScore();
+                //InternationalStudent set functions
+                void setCountry(string Cnt);
 
 };
 
+//-------------------------DomesticStudent----------------------
+
 class DomesticStudent: public Student
 {
+        //DomesticStudent private variable
         private:
                 string Province;
+        //DomesticStudent public functions
         public:
+        //DomesticStudent default function
                 DomesticStudent(string PRV);
                 DomesticStudent();
+        //DomesticStudent get function
                 string getProvince();
+        //DomesticStudent set function
                 void setProvince(string PRV);
 
 };
